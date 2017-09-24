ce88488c48850fe7af5f3ae867d104a1a36a5c90
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-09-24 00:52:13 -0500

    add level 1 state, title, and start button

--- public/main.js
+++ public/main.js
@@ -1,8 +1,29 @@
-var game = new Phaser.Game(1920, 1080, Phaser.AUTO, 'game-root');
+var width = 1920;
+var height = 1080;
+var game = new Phaser.Game(width, height, Phaser.AUTO, 'game-root');
+
+var textStyle = {
+  fill: 'white'
+};
 
 game.state.add('menu', {
-  create: function() {
+  create: function () {
     game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+    var title = game.add.text(width / 2, height / 2, 'WELLSPRING', Object.assign(textStyle, {
+      fontSize: '120px',
+    }));
+    title.anchor.set(0.5);
+
+    var startButton = game.add.button(width / 2, height * 0.7, 'START', startGame)
+    function startGame() {
+      game.state.start('level1');
+    }
+  }
+});
+
+game.state.add('level1', {
+  create: function () {
+    game.add.text(game.world.centerX, game.world.centerY, 'Level 1', textStyle);
   }
 });
 
