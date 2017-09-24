780dac266d58e575c748c7e3dccfac02315918db
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-09-24 18:27:31 -0500

    fine-tune jump mechanic

--- public/main.js
+++ public/main.js
@@ -26,18 +26,23 @@ game.state.add('menu', {
   }
 });
 
-var player;
+var player, ground;
 
 game.state.add('level1', {
   preload: function () {
     game.load.image('player', 'sprites/player.jpg');
+    game.load.image('well_bottom', 'sprites/well_bottom.png');
   },
   create: function () {
     game.physics.startSystem(Phaser.Physics.ARCADE);
     applyGameSettings();
     player = new Player(game.world.centerX, height - 20);
+    ground = game.add.sprite(0, height * 0.9, 'well_bottom');
+    game.physics.enable(ground, Phaser.Physics.ARCADE);
+    ground.body.immovable = true;
   },
   update: function() {
+    game.physics.arcade.collide(player.sprite, ground);
     player.readInput();
   }
 });
--- public/modules/Player.js
+++ public/modules/Player.js
@@ -1,6 +1,6 @@
 function Player(x, y) {
-  this.speed = 5;
-  this.jumpPower = 800;
+  this.baseSpeed = 500;
+  this.jumpPower = 1000;
   this.sprite = game.add.sprite(x, y, 'player');
   this.sprite.anchor.x = 0.5;
   this.sprite.anchor.y = 1;
@@ -9,16 +9,37 @@ function Player(x, y) {
   game.physics.enable(this.sprite, Phaser.Physics.ARCADE);
   this.sprite.body.collideWorldBounds = true;
   this.sprite.body.gravity.y = 1500;
+  this.sprite.body.maxVelocity.x = this.baseSpeed;
 }
+
+Object.defineProperties(Player.prototype, {
+  onGround: {
+    get: function () {
+      return this.sprite.body.onFloor() || this.sprite.body.touching.down;
+    },
+    set: function (y) {
+    }
+  },
+  speed: {
+    get: function () {
+      // Move slower in the air
+      return this.baseSpeed * (this.onGround ? 1 : 0.05);
+    }
+  }
+});
+
 Player.prototype.readInput = function () {
+  // Player is standing on ground
+  if (this.onGround) {
+    this.sprite.body.velocity.x = 0;
+    if (game.input.keyboard.isDown(Phaser.Keyboard.UP)) {
+      this.sprite.body.velocity.y = -this.jumpPower;
+    }
+  }
   if (game.input.keyboard.isDown(Phaser.Keyboard.LEFT)) {
-    this.sprite.x -= this.speed;
+    this.sprite.body.velocity.x -= this.speed;
   }
   if (game.input.keyboard.isDown(Phaser.Keyboard.RIGHT)) {
-    this.sprite.x += this.speed;
-  }
-  if (game.input.keyboard.isDown(Phaser.Keyboard.UP) &&
-      this.sprite.body.onFloor()) {
-    this.sprite.body.velocity.y = -this.jumpPower;
+    this.sprite.body.velocity.x += this.speed;
   }
 };
