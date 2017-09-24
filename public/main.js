6249e93a312544380a74b0cfc40f0327a3518c34
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-09-24 00:39:30 -0500

    make game resizeable and add first state

--- public/main.js
+++ public/main.js
@@ -1,1 +1,9 @@
 var game = new Phaser.Game(1920, 1080, Phaser.AUTO, 'game-root');
+
+game.state.add('menu', {
+  create: function() {
+    game.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+  }
+});
+
+game.state.start('menu');
