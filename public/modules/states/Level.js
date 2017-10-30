ab7162e7b0a4929f6da7e39d86e947c6f4503c00
Mason Bourgeois <mason.bourgeois@gmail.com>
2017-10-30 13:58:20 -0500

    1 tile = 1 foot

--- public/modules/states/Level.js
+++ public/modules/states/Level.js
@@ -28,7 +28,7 @@ Object.defineProperties(Level.prototype, {
             reduce(function (total, current) {
               return total + current.height;
             }, 0);
-          this.bottomSign = new DepthSign(game.world.centerX + 100, game.world.height - 28, totalHeight);
+          this.bottomSign = new DepthSign(game.world.centerX + 100, game.world.height - 28, totalHeight / 32);
 
           if (this.callbacks.hasOwnProperty('create')) {
             this.callbacks.create.call(this);
