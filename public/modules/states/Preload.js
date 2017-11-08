52316dc11e7334d672b5dea550e294ca2a88cf44
kellyasim <kelly.sim@utexas.edu>
2017-11-08 14:08:57 -0600

    switched vine sprite

--- public/modules/states/Preload.js
+++ public/modules/states/Preload.js
@@ -39,8 +39,8 @@ var preload = {
     game.load.image('wellBottom','tilemaps/stoneledge.png');
     game.load.image('ladder','tilemaps/ladder.png');
     game.load.image('wallTile','tilemaps/wallTile.png');
-    game.load.image ("vineTile", "tilemaps/vines.png");
-    ///game.load.image('vineTile', 'tilemaps/vineTile.png');
+    ///game.load.image ("vineTile", "tilemaps/vines.png");
+    game.load.image('vineTile', 'tilemaps/vineTile.png');
     game.load.image('arrow_keys', 'sprites/movekeys.png');
     game.load.image("up_arrow", "sprites/up.png");
     game.load.image('sign', 'sprites/sign.png');
